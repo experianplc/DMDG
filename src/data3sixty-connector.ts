9b2925d5cf5a95f97f29cda9435d627c63deae42
Tabeth Nkangoh <tabeth@tabeth.com>
2019-04-26 12:33:24 -0400

    Profiles now works | AB#113433

--- src/data3sixty-connector.ts
+++ src/data3sixty-connector.ts
@@ -57,6 +57,30 @@ interface PandoraRule {
   "RULE THRESHOLD": string;
 }
 
+interface PandoraProfile {
+  "ROW COUNT": string;
+  "UNIQUENESS": string;
+  "SCALE": string;
+  "AVERAGE": string;
+  "STANDARD DEVIATION OF VALUES": string;
+  "PRECISION": string;
+  "DOMINANT DATATYPE": string;
+  "MINIMUM": string;
+  "MAXIMUM": string;
+  "MOST COMMON VALUE": string;
+  "COUNT OF MOST COMMON VALUES": string;
+  "ID": string;
+  "NAME": string;
+  "UNIQUE COUNT": string;
+  "COMPLETENESS": string;
+  "NULL COUNT": string;
+  "EXTERNAL DATABASE": string;
+  "SCHEMA EXTERNAL NAME": string;
+  "TABLE EXTERNAL NAME": string;
+  "EXTERNAL COLUMN NAME": string;
+  "EXTERNAL NAME": string;
+}
+
 interface NormalizedAssetProperties {
   Instance: string; 
   Schema: string;
@@ -371,37 +395,7 @@ export class Data3SixtyConnector extends Connector {
   sendDataQualityRules(): PromiseLike<any>  {
     return this.preSendDataQualityRules().then(() => {
       return new Promise((resolve: any, reject: any) => {
-        let month = String(this.lastRun.getMonth() + 1);
-        if (month.length === 1) {
-          month = "0" + month;
-        }
-
-        let day = String(this.lastRun.getDate());
-        if (day.length === 1) {
-          day = "0" + day;
-        }
-
-        let year = String(this.lastRun.getFullYear());
-        if (year.length === 1) {
-          year = "0" + year;
-        }
-
-        let hours = String(this.lastRun.getHours());
-        if (hours.length === 1) {
-          hours = "0" + hours;
-        }
-
-        let minutes = String(this.lastRun.getMinutes());
-        if (minutes.length === 1){
-          minutes = "0" + minutes;
-        }
-
-        let milliseconds = String(this.lastRun.getMilliseconds());
-        if (milliseconds.length === 1) {
-          milliseconds = "0" + milliseconds;
-        }
-
-        const safeDateTimeString = `${year}-${month}-${day} ${hours}:${minutes}:${milliseconds}`;
+        const safeDateTimeString = this._getSafeDateTimeString();
 
         axios.request({
           url: `http://${this.odbcUrl}/query`,
@@ -502,6 +496,148 @@ export class Data3SixtyConnector extends Connector {
     });
   }
 
+
+  preSendDataQualityProfiles(): PromiseLike<any> {
+    return new Promise((resolve: any, reject: any) => {
+      resolve(null);
+    });
+  }
+
+  sendDataQualityProfiles(): PromiseLike<any> {
+    return this.preSendDataQualityProfiles().then(() => {
+      return new Promise((resolve: any, reject: any) => {
+        axios.request({ 
+          url: `http://${this.odbcUrl}/query`,
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          data: {
+            sql: `SELECT * FROM "COLUMNS"`,
+          }
+        }).then((profileData) => {
+          if (profileData.data.length === 0) {
+            console.log("No data found (either no rules have been created, or no new rules have been validated)");
+          }
+
+          let postPromises: PromiseLike<any>[] = [];
+
+          for (let i = 0; i < profileData.data.length; i++) {
+            const rule: PandoraProfile = profileData.data[i];
+
+            const database = rule["EXTERNAL DATABASE"].toLocaleLowerCase();
+            const schema = rule["SCHEMA EXTERNAL NAME"].toLocaleLowerCase();
+            const table = rule["TABLE EXTERNAL NAME"].toLocaleLowerCase();
+            const column = rule["EXTERNAL NAME"].toLocaleLowerCase();
+
+            let uid: string;
+            try  {
+              uid = this.assetMetaDataToTechnologyAssetUuid[database][schema][table][column];
+            } catch(e) {
+              console.log(`Mapping not found to: ${database}-${schema}-${table}-${column}, continuing...`);
+              continue;
+            }
+
+            console.log(`Rule found: ${rule}`);
+            const postPromise = new Promise((resolve, reject) => {
+              axios.request({
+                url: `${this.url}/api/v2/profiles/`,
+                method: "POST",
+                headers: {
+                  "Authorization": `${this.apiKey};${this.apiSecret}`
+                },
+                data: [
+                  {
+                    "AssetUid": uid,
+                    "RowCount": rule["ROW COUNT"],
+                    "Uniqueness": rule["UNIQUENESS"],
+                    "UniqueCount": rule["UNIQUE COUNT"],
+                    "Completeness": rule["COMPLETENESS"],
+                    "NullCount": rule["NULL COUNT"],
+                    "BlankCount": rule["NULL COUNT"],
+                    "DataType": rule["DOMINANT DATATYPE"],
+                    "MinimumValue": rule["MINIMUM"],
+                    "MaximumValue": rule["MAXIMUM"],
+                    "Precision": rule["PRECISION"],
+                    "Scale": rule["SCALE"],
+                    "Average": rule["AVERAGE"],
+                    "Median": 0, /* Pandora doesn't support this */
+                    "StandardDeviation": rule["STANDARD DEVIATION OF VALUES"],
+                    "Top10Values": [
+                      {
+                        "Value": rule["MOST COMMON VALUE"],
+                        "Count": rule["COUNT OF MOST COMMON VALUES"]
+                      }
+                    ],
+                    "ProcessIdentifier": rule["ID"]
+                  }
+                ]
+              }).then((data) => {
+                resolve(data);
+                console.log(`PROFILE DATA FOR: '${rule["NAME"]}' sent successfully.`)
+              }).catch((err) => {
+                reject(err);
+                console.log(err);
+              })
+            });
+
+            postPromises.push(postPromise);
+          }
+
+          Promise.all(postPromises).then((data) => {
+            if (data.length > 0) {
+              this.configuration.set("Data3SixtyConnector.lastRun", new Date().toString());
+              this.configuration.save();
+            }
+
+            this.postSendDataQualityRules().then(() => {
+            });
+          });
+
+
+        });
+      })
+    });
+  }
+
+  postSendDataQualityProfiles(): PromiseLike<any> {
+    return new Promise((resolve: any, reject: any) => {
+      resolve(null);
+    });
+  }
+
+  private _getSafeDateTimeString(): string {
+    let month = String(this.lastRun.getMonth() + 1);
+    if (month.length === 1) {
+      month = "0" + month;
+    }
+
+    let day = String(this.lastRun.getDate());
+    if (day.length === 1) {
+      day = "0" + day;
+    }
+
+    let year = String(this.lastRun.getFullYear());
+    if (year.length === 1) {
+      year = "0" + year;
+    }
+
+    let hours = String(this.lastRun.getHours());
+    if (hours.length === 1) {
+      hours = "0" + hours;
+    }
+
+    let minutes = String(this.lastRun.getMinutes());
+    if (minutes.length === 1){
+      minutes = "0" + minutes;
+    }
+
+    let milliseconds = String(this.lastRun.getMilliseconds());
+    if (milliseconds.length === 1) {
+      milliseconds = "0" + milliseconds;
+    }
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${milliseconds}`;
+  }
+
   private _normalizeJsonString(jsonString: string): NormalizedAssetProperties {
     return tailored.defmatch(
       tailored.clause([$], (value: string) => {
@@ -515,7 +651,9 @@ export class Data3SixtyConnector extends Connector {
   }
 };
 
+// Send rule data and profile data over.
 const runner = new Data3SixtyConnector();
 runner.retrieveAssets().then(() => {
-  runner.sendDataQualityRules()
+  runner.sendDataQualityRules();
+  runner.sendDataQualityProfiles();
 })
